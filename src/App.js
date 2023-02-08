2cd2444e42514a74497a00fe66dffee2ee49c054
Yuliia Butenko <yuliiabutenko@yahoo.com>
2023-02-08 12:32:03 -0600

    added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import { Fragment, useEffect } from "react";
 import Layout from "./components/layout/Layout";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/homepage/HomePage";
 
 function App() {
   return (
     <Fragment>
-      <Layout></Layout>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/Layout" element={<Layout />} />
+        </Routes>
+      </Router>
     </Fragment>
   );
 }
