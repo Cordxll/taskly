06996dded15e22dbdde9763f51b84299e68b0c2c
Cordell Browne <cordell@knights.ucf.edu>
2023-02-19 12:52:12 -0500

    Home

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { Fragment, useEffect } from "react";
 import Layout from "./components/layout/Layout";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CalendarAndTasks from "./components/calendar/CalendarAndTasks";
-import HomePage from "./components/homepage/HomePage";
 import EditCard from "./components/goals/actions/EditCard";
+import HomePublic from "./components/home/HomePublic";
 
 function App() {
   return (
     <Fragment>
       <Router>
         <Routes>
+          <Route exact path="/" element={<HomePublic/>}/>
           <Route exact path="/Layout" element={<Layout />} />
           <Route exact path="/Edit" element={<EditCard />} />
           <Route exact path="/Cal" element={<CalendarAndTasks />} />
--- src/components/home/HomePublic.js
+++ src/components/home/HomePublic.js
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom'
 import classes from './Home.module.css'
 export default  function  HomePublic(){
   return(
     <div className={classes.container} style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
       <div style={{alignContent:"center",textAlign:"center",width:'90%'}}>
-        <img 
+       <img 
         src='https://www.notion.so/cdn-cgi/image/format=auto,width=640,quality=100/front-static/pages/product/home-page-hero-refreshed-v3.png'
         className={classes.img}/>
         <h1 className={classes.h1}>
@@ -14,6 +15,9 @@ export default  function  HomePublic(){
           <button className={classes.button} style={{backgroundColor:"black",color:"white"}}>Sign Up</button>
           <button className={classes.button} style={{backgroundColor:"white"}}>Sign In</button>
         </div>
+        <Link to='/Cal'>Calendar</Link>
+        <Link to='/Edit'>Edit</Link>
+        <Link to='/Layout'>Layout</Link>
       </div>    
     </div>
   )
