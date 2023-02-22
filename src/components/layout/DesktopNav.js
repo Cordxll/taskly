7bec8110be4f9e03fe7d3cbe595a71474c4936b1
Cordell Browne <cordell@knights.ucf.edu>
2023-02-22 13:49:52 -0500

    Desktop Nav prototype

--- src/components/layout/DesktopNav.js
+++ src/components/layout/DesktopNav.js
@@ -1,11 +1,18 @@
 import { Fragment } from "react";
 import classes from "./DesktopNav.module.css";
-import Home from "../pics/home.svg";
 import Calendar from "../pics/calendar.svg";
 import Profile from "../pics/profile.svg";
 import Stats from "../pics/stats.svg";
 import { useDispatch } from "react-redux";
 import { addActions } from "../../store/addSlice";
+
+//https://react-icons.github.io/react-icons to search for icons 
+import {BiHome,BiCalendarCheck,BiLogOut} from 'react-icons/bi'
+import {MdAddBox,MdPersonOutline,MdOutlineLogout} from 'react-icons/md'
+import {VscGraph} from 'react-icons/vsc'
+import {TbLogout} from 'react-icons/tb'
+import { Link } from "react-router-dom";
+
 const DesktopNav = () => {
   const dispatch = useDispatch();
   const toggleSaveFormHandler = () => {
@@ -15,30 +22,39 @@ const DesktopNav = () => {
   return (
     <Fragment>
       <div className={classes.navigation}>
-        <button className={classes.homeButton}>
-          <img className={classes.icon} src={Home} alt="homepage" />
-          <span>Home</span>
-        </button>
-        <button className={classes.calendarButton}>
-          <img className={classes.icon} src={Calendar} alt="calendar" />
-          <span>Calendar</span>
-        </button>
-        <button className={classes.addButton} onClick={toggleSaveFormHandler}>
-          <div className={classes.circle_plus}>
-            <div className={classes.circle}>
-              <div className={classes.horizontal}></div>
-              <div className={classes.vertical}></div>
-            </div>
+        <div style={{height:'100%',}}>
+          <div className={classes.subcontainer}>
+            <button className={`${classes.plus} ${classes.link}`} onClick={toggleSaveFormHandler}>
+              New Event
+            </button>
           </div>
-        </button>
-        <button className={classes.statsButton}>
-          <img className={classes.icon} src={Stats} alt="statistics" />
-          <span>Statistics</span>
-        </button>
-        <button className={classes.profileButton}>
-          <img className={classes.icon} src={Profile} alt="profile" />
-          <span>Profile</span>
-        </button>
+          
+          <div className={classes.subcontainer}>
+            <Link to="/" className={classes.link}>
+              <BiHome className={classes.icon}/>
+              <p>Home</p>
+            </Link>
+            <Link to="/cal" className={classes.link}>
+              <BiCalendarCheck className={classes.icon}/>
+              <p>Calendar</p>
+            </Link>
+            <Link to="/statistics" className={classes.link}>
+              <VscGraph className={classes.icon}/>
+              <p>Statistics</p>
+            </Link>
+          </div>
+        </div>
+
+        <div className={`${classes.subcontainer} ${classes.last}`}>
+          <Link to="/profile" className={classes.link}>
+            <MdPersonOutline className={classes.icon}/>
+            <p>Profile</p>
+          </Link>
+          <button className={classes.link}>
+            <MdOutlineLogout className={classes.icon}/>
+            <p>Logout</p>
+          </button>
+        </div>
       </div>
     </Fragment>
   );
